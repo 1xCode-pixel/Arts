a379578a997d030ac81276d9f4b11b526f327437
Артём Березовских <1xcodeteam@gmail.com>
2026-03-29 10:47:19 +0500

    Update Firebase API key in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "ВАШ_API_KEY",
+    apiKey: "AIzaSyC_ztsyKmGRriSJ_iJlgTJCUq_AvxOqHOM",
     authDomain: "ВАШ_PROJECT.firebaseapp.com",
     projectId: "ВАШ_PROJECT_ID",
     storageBucket: "ВАШ_PROJECT.appspot.com",
